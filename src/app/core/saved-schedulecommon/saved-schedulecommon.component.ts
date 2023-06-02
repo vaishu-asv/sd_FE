0fdf708f133eda55dc982536cb179bcd6c96f48e
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-02 18:09:07 +0530

    Initial commit

---
+++ src/app/core/saved-schedulecommon/saved-schedulecommon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-schedulecommon',
+  templateUrl: './saved-schedulecommon.component.html',
+  styleUrls: ['./saved-schedulecommon.component.scss']
+})
+export class SavedSchedulecommonComponent {
+
+}
